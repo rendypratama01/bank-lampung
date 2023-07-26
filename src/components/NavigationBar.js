ebbdf0324711e8e7a5e2cc286c9ba6a74b80003e
satriaberliano <sanozisb@gmail.com>
2023-07-26 11:20:02 +0700

    add : style when NavLink click

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -6,16 +6,18 @@ import {
   NavDropdown,
 } from "react-bootstrap";
 import logo from "../assets/img/logo.png";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import {
   aboutPath,
   homePath,
   contactPath,
-  layananPath,
   rootPath,
+  servicePath,
 } from "../routes";
 
 function NavigationBar() {
+  const location = useLocation();
+
   return (
     <div>
       <Navbar bg="light" data-bs-theme="light" fixed="top">
@@ -30,10 +32,29 @@ function NavigationBar() {
             />
           </NavbarBrand>
           <Nav>
-            <NavLink to={homePath}>Beranda</NavLink>
-            <NavLink to={aboutPath}>Tentang</NavLink>
+            <NavLink
+              to={rootPath}
+              className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "active" : ""
+              }
+            >
+              Beranda
+            </NavLink>
+            <NavLink
+              to={aboutPath}
+              className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "active" : ""
+              }
+            >
+              Tentang
+            </NavLink>
             <NavDropdown title="Produk" id="basic-nav-dropdown">
-              <NavDropdown.Item href={layananPath}>Layanan</NavDropdown.Item>
+              <NavDropdown.Item
+                href={servicePath}
+                className={location.pathname === servicePath ? "active" : ""}
+              >
+                Layanan
+              </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
               </NavDropdown.Item>
@@ -42,7 +63,12 @@ function NavigationBar() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
-            <NavLink to={contactPath}>Contact</NavLink>
+            <NavLink
+              to={contactPath}
+              className={location.pathname === contactPath ? "active" : ""}
+            >
+              Contact
+            </NavLink>
           </Nav>
         </Container>
       </Navbar>
