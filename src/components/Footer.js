af3aa87a6a58b6c0cd77ab656a5f95264c753c83
satriaberliano <sanozisb@gmail.com>
2023-08-01 11:42:46 +0700

    delete : connect apps

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,34 +17,7 @@ export default function Footer() {
         bgColor="light"
         className="text-center text-lg-start text-muted"
       >
-        <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-          <div className="me-5 d-none d-lg-block">
-            <span>Get connected with us on social networks:</span>
-          </div>
-
-          <div>
-            <a href="#home" className="me-4 text-reset">
-              <MDBIcon color="secondary" fab icon="facebook-f" />
-            </a>
-            <a href="#home" className="me-4 text-reset">
-              <MDBIcon color="secondary" fab icon="twitter" />
-            </a>
-            <a href="#home" className="me-4 text-reset">
-              <MDBIcon color="secondary" fab icon="google" />
-            </a>
-            <a href="#home" className="me-4 text-reset">
-              <MDBIcon color="secondary" fab icon="instagram" />
-            </a>
-            <a href="#home" className="me-4 text-reset">
-              <MDBIcon color="secondary" fab icon="linkedin" />
-            </a>
-            <a href="#home" className="me-4 text-reset">
-              <MDBIcon color="secondary" fab icon="github" />
-            </a>
-          </div>
-        </section>
-
-        <section className="">
+        <section className="footer-info">
           <MDBContainer className="text-center text-md-start mt-5">
             <MDBRow className="mt-3">
               <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
@@ -57,9 +30,9 @@ export default function Footer() {
                   />
                 </h6>
                 <p>
-                  Here you can use rows and columns to organize your footer
-                  content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                  elit.
+                  Bank Lampung KCP Menggala adalah salah satu cabang Bank
+                  Pembangunan Daerah (BPD) yang berlokasi di daerah Menggala,
+                  Lampung.
                 </p>
               </MDBCol>
 
@@ -109,15 +82,11 @@ export default function Footer() {
                 </p>
                 <p>
                   <MDBIcon color="secondary" icon="envelope" className="me-3" />
-                  info@example.com
+                  komunikasi@banklampung.co.id
                 </p>
                 <p>
-                  <MDBIcon color="secondary" icon="phone" className="me-3" /> +
-                  01 234 567 88
-                </p>
-                <p>
-                  <MDBIcon color="secondary" icon="print" className="me-3" /> +
-                  01 234 567 89
+                  <MDBIcon color="secondary" icon="phone" className="me-3" />
+                  1500575
                 </p>
               </MDBCol>
             </MDBRow>
