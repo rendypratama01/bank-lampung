254fd39b6d8106130298e1462aca1b7ce056c9e9
satriaberliano <sanozisb@gmail.com>
2023-08-02 09:39:48 +0700

    add : pinjaman page and content

---
+++ src/components/Pinjaman.js
@@ -0,0 +1,268 @@
+import React from "react";
+import { Card, Col, Row } from "react-bootstrap";
+import kreditAnekaUsaha from "../assets/img/pinjaman/kredit-aneka-usaha.png";
+import kreditKontraktor from "../assets/img/pinjaman/kredit-kontraktor.png";
+import kreditKmkLainnya from "../assets/img/pinjaman/kredit-kmk-lainnya.png";
+import kmkKiKur from "../assets/img/pinjaman/kmk-ki-kur.png";
+import kreditResiGudang from "../assets/img/pinjaman/kredit-resi-gudang.png";
+import kreditPantasPegawaiAktif from "../assets/img/pinjaman/kredit-pantas-pegawai-aktif.png";
+import kreditPantasPraPensiun from "../assets/img/pinjaman/kredit-pantas-pra-pensiun.png";
+import kreditPantasPensiun from "../assets/img/pinjaman/kredit-pantas-pensiun.png";
+import kreditSigerDewan from "../assets/img/pinjaman/kredit-siger-dewan.png";
+import kreditPerorangan from "../assets/img/pinjaman/kredit-perorangan.png";
+import kreditLinkageProgram from "../assets/img/pinjaman/kredit-linkage-program.png";
+import kmkKonstruksiDeveloper from "../assets/img/pinjaman/k-m-k-konstruksi-developer.png";
+import kreditUsahaRakyat from "../assets/img/pinjaman/kredit-usaha-rakyat(-k-u-r).png";
+import rekeningKoran from "../assets/img/pinjaman/rekening-koran.png";
+import bankGaransi from "../assets/img/pinjaman/bank-garansi.png";
+
+function Pinjaman() {
+  return (
+    <Row xs={1} md={4} className="g-4">
+      <Col>
+        <Card>
+          <Card.Img
+            variant="top"
+            src={kreditAnekaUsaha}
+            alt="Kredit aneka usaha"
+          />
+          <Card.Body>
+            <Card.Title>Kredit Aneka Usaha</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card>
+          <Card.Img
+            variant="top"
+            src={kreditKontraktor}
+            alt="Kredit kontraktor"
+          />
+          <Card.Body>
+            <Card.Title>Kredit Kontraktor</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card>
+          <Card.Img
+            variant="top"
+            src={kreditKmkLainnya}
+            alt="Kredit KI KMK lainnya"
+          />
+          <Card.Body>
+            <Card.Title>Kredit KI/KMK Lainnya</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card>
+          <Card.Img variant="top" src={kmkKiKur} alt="KMK KI KUR" />
+          <Card.Body>
+            <Card.Title>KMK/KI-KUR</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card>
+          <Card.Img
+            variant="top"
+            src={kreditResiGudang}
+            alt="Kredit resi gudang"
+          />
+          <Card.Body>
+            <Card.Title>Kredit Resi Gudang</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card>
+          <Card.Img
+            variant="top"
+            src={kreditPantasPegawaiAktif}
+            alt="Kredit pantas pegawai aktif"
+          />
+          <Card.Body>
+            <Card.Title>Kredit Pantas Pegawai Aktif</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card>
+          <Card.Img
+            variant="top"
+            src={kreditPantasPraPensiun}
+            alt="Kredit pantas pra pensiun"
+          />
+          <Card.Body>
+            <Card.Title>Kredit Pantas Pra Pensiun</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card>
+          <Card.Img
+            variant="top"
+            src={kreditPantasPensiun}
+            alt="Kredit pantas pensiun"
+          />
+          <Card.Body>
+            <Card.Title>Kredit Pantas Pensiun</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card>
+          <Card.Img
+            variant="top"
+            src={kreditSigerDewan}
+            alt="Kredit siger dewan"
+          />
+          <Card.Body>
+            <Card.Title>Kredit Siger Dewan</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card>
+          <Card.Img
+            variant="top"
+            src={kreditPerorangan}
+            alt="Kredit perorangan"
+          />
+          <Card.Body>
+            <Card.Title>Kredit Perorangan</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card>
+          <Card.Img
+            variant="top"
+            src={kreditLinkageProgram}
+            alt="Kredit linkage program"
+          />
+          <Card.Body>
+            <Card.Title>Kredit Linkage Program</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card>
+          <Card.Img
+            variant="top"
+            src={kmkKonstruksiDeveloper}
+            alt="KMK konstruksi developer"
+          />
+          <Card.Body>
+            <Card.Title>KMK Konstruksi Developer</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card>
+          <Card.Img
+            variant="top"
+            src={kreditUsahaRakyat}
+            alt="Kredit usaha rakyat"
+          />
+          <Card.Body>
+            <Card.Title>Kredit Usaha Rakyat</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card>
+          <Card.Img variant="top" src={rekeningKoran} alt="Rekening koran" />
+          <Card.Body>
+            <Card.Title>Rekening Koran</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col>
+        <Card>
+          <Card.Img variant="top" src={bankGaransi} alt="Bank garansi" />
+          <Card.Body>
+            <Card.Title>Bank Garansi</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );
+}
+
+export default Pinjaman;
---
+++ src/pages/PinjamanPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Pinjaman from "../components/Pinjaman";
+
+function PinjamanPage() {
+  return (
+    <>
+      <div className="pinjaman-container">
+        <h3>Pinjaman</h3>
+        <Pinjaman />
+      </div>
+    </>
+  );
+}
+
+export default PinjamanPage;
